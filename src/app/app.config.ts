78f9d2706f2436bacd0542ea6fca0247c3c9e6c2
Nicolas Nahuel Gonzalez <nicolasgonzalez470@gmail.com>
2025-10-03 18:02:28 -0300

    primer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideAnimationsAsync(),
+    importProvidersFrom(HttpClientModule),
+    providePrimeNG({
+        theme: {
+            preset: Aura
+        }
+    }),
+    provideHttpClient()
   ]
 };
